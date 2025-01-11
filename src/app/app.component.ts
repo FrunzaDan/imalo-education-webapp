6145e4195d1b912242700596f0d77ef10a51ce26
FrunzaDan <frunzadan96@gmail.com>
2025-01-12 01:18:31 +0200

    scholars table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GanttChartComponent } from './components/gantt-chart/gantt-chart.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [GanttChartComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ScholarTableComponent } from './components/scholar-table/scholar-table.component';
+import { GanttChartComponent } from './components/gantt-chart/gantt-chart.component';
+import { AttendanceComponent } from './components/attendance/attendance.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ScholarTableComponent },
+  { path: 'scholars', component: ScholarTableComponent },
+  { path: 'pickup-time', component: GanttChartComponent },
+  { path: 'attendance', component: AttendanceComponent },
+];
