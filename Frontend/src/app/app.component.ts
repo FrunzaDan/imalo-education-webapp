ecd4e31c3bfc3ad88899f3e48b106200d734eb19
FrunzaDan <frunzadan96@gmail.com>
2025-06-08 16:28:09 +0300

    create scholar in progress 2

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GanttChartComponent } from './components/gantt-chart/gantt-chart.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ImaloEducationWebapp';
+  environment = environment;
+  apiAvailable = true;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Check API health by pinging endpoint
+    this.http.get(environment.baseUrlScholars).subscribe({
+      next: () => (this.apiAvailable = true),
+      error: () => (this.apiAvailable = false),
+    });
+  }
 }
